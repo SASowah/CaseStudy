ef236173c5ba187786b3e7cda4705e319e2ada15
Samuel A. Sowah <160193749+SASowah@users.noreply.github.com>
2024-02-15 21:25:22 -0500

    Update and rename Bikeshare.Prepare-Process.sql to Bikeshare.CreateTable.sql

---
+++ Bikeshare.CreateTable.sql
@@ -0,0 +1,163 @@
+--Before bringing the csv file in.Create table with list of all the columns
+CREATE TABLE bikeshare_data03(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data04(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+CREATE TABLE bikeshare_data05(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data06(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data07(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data08(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data09(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data10(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data11(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+CREATE TABLE bikeshare_data12(
+    ride_id VARCHAR(255) PRIMARY KEY,
+    rideable_type VARCHAR(255),
+    started_at VARCHAR(255),
+    ended_at VARCHAR(255),
+    start_station_name VARCHAR(255),
+    start_station_id VARCHAR(255),
+    end_station_name VARCHAR(255),
+    end_station_id VARCHAR(255),
+    start_lat VARCHAR(255),
+    start_lng VARCHAR(255),
+    end_lat VARCHAR(255),
+    end_lng VARCHAR(255),
+    member_casual VARCHAR(255)
+);
+
+SELECT * FROM public.bikeshare_data12
+ORDER BY ride_id ASC ;
+
